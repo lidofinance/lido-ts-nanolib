3060909223b1e81e81fbc5c86339be486360a28a
Eddort <webisforce@gmail.com>
2022-12-03 21:11:06 +0400

    feat: spec & chain operator

--- src/validator/errors.ts
+++ src/validator/errors.ts
@@ -1,1 +1,8 @@
 export class ValidationError extends Error {}
+export const makeError = (message: string | number | undefined, defaultMessage: string) => {
+  const msg =
+    typeof message === 'undefined' || typeof message === 'number'
+      ? defaultMessage
+      : message
+  throw new ValidationError(msg)
+}
--- src/validator/operators.ts
+++ src/validator/operators.ts
@@ -10,9 +10,11 @@ export const optional = (cb: ReturnType<typeof make>) => {
 
 type Fn<T> = (arg: T) => T
 type Chain<T> = Fn<T>[]
-export const chain = <T>(arg: T, ...fns: Chain<T>) => {
-  for (let i = fns.length - 1; i >= 0; i--) {
-    arg = fns[i](arg)
-  }
-  return arg
-}
+// export const chain = <T>(arg: T, ...fns: Chain<T>) => {
+//   for (let i = fns.length - 1; i >= 0; i--) {
+//     arg = fns[i](arg)
+//   }
+//   return arg
+// }
+export const chain = <T>(arg: T, ...fns: Chain<T>) =>
+  fns.reduce((acc, fn) => fn(acc), arg)
