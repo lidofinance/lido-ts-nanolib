88a430a4c2e5f597cee63863b5a42ef6ba0e769c
Eddort <webisforce@gmail.com>
2022-12-03 15:04:11 +0400

    feat: deep values validation

--- src/validator/index.ts
+++ src/validator/index.ts
@@ -1,40 +1,5 @@
-import { ValidationError } from './errors.js'
 import * as v from './validators.js'
-
-export const make = <T>(
-  parseFn: (input: unknown, errorMessage?: string | number) => T
-) => {
-  return (str?: unknown, errorMessage?: string| number) => {
-    if (typeof str === 'undefined')
-      throw new ValidationError(
-        typeof errorMessage === 'undefined' || typeof errorMessage === 'number'
-          ? 'Empty value'
-          : errorMessage
-      )
-    return parseFn(str, errorMessage)
-  }
-}
-
-export const makeDeep = <T>(
-  parseFn: (input: unknown, errorMessage?: string) => T
-) => {
-  return <B>(str: unknown, cb: (arg: T) => B, errorMessage?: string) => {
-    if (typeof str === 'undefined')
-      throw new ValidationError(errorMessage || 'Empty value')
-
-    return cb(parseFn(str, errorMessage))
-  }
-}
-
-export const makeOptional = <T>(parseFn: (input: unknown) => T) => {
-  return (str?: unknown, cb?: <A>(arg: T) => A) => {
-    if (typeof str === 'undefined') return
-    try {
-      if (cb) return cb(parseFn(str))
-      return parseFn(str)
-    } catch (_) {}
-  }
-}
+import { make, makeOptional, makeDeep, makeOptionalDeep } from './makers.js'
 
 export const num = make(v.num)
 export const optional_num = makeOptional(v.num)
@@ -49,19 +14,34 @@ export const level_attr = make(v.level_attr)
 export const optional_level_attr = makeOptional(v.level_attr)
 
 export const obj = makeDeep(v.obj)
-export const optional_obj = makeOptional(v.obj)
+export const optional_obj = makeOptionalDeep(v.obj)
 
 export const arr = makeDeep(v.arr)
-export const optional_arr = makeOptional(v.arr)
+export const optional_arr = makeOptionalDeep(v.arr)
 
+const validateResponse = (json: unknown) =>
+  obj(json, (object) => ({
+    title: str(object.title, 'Empty title'),
+    user: obj(object.user, (user) => ({
+      name: str(user.name),
+      surname: str(user.surname),
+    })),
+    projectNames: arr(
+      object.projectNames,
+      (p) => p.map(str),
+      'Invalid array project'
+    ),
+  }))
 
 const obje = {
   title: str(obj, 'Empty title'),
-  user: obj('sdasdasd', (user) => ({
+  user: optional_obj('sdasdasd', (user) => ({
     name: str(user.name),
     surname: str(user.surname),
   })),
   projectNames: arr(str, (p) => p.map(str), 'Invalid array project'),
 }
 const s = str('')
-obje.user.name
+if (obje.user) {
+  obje.user.name
+}
