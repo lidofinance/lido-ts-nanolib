1aa21b0bbd638a7c4b92f11fe0612fab013a8e9f
Eddort <webisforce@gmail.com>
2022-12-05 14:49:34 +0400

    feat: wrap operator

--- src/validator/operators.ts
+++ src/validator/operators.ts
@@ -1,3 +1,5 @@
+import { ValidationError } from './errors'
+
 export const optional = <T>(cb: () => T) => {
   try {
     return cb()
@@ -6,46 +8,28 @@ export const optional = <T>(cb: () => T) => {
   }
 }
 
+export const wrap = <T>(cb: () => T, errorMessage?: string) => {
+  try {
+    return cb()
+  } catch (error) {
+    throw new ValidationError(errorMessage || error.message)
+  }
+}
+
 type Fn<T> = (arg: T) => T
 type Chain<T> = Fn<T>[]
-// export const chain = <T>(arg: T, ...fns: Chain<T>) => {
-//   for (let i = fns.length - 1; i >= 0; i--) {
-//     arg = fns[i](arg)
-//   }
-//   return arg
-// }
-// export const chain =
-//   <T>(...fns: Chain<T>) =>
-//   <B>(arg: B extends T ? B : never) =>
-//     fns.reduce((acc, fn) => fn(acc), arg)
-export const chain =
-  <T>(...fns: Chain<T>) =>
-  (arg: T) =>
-    fns.reduce((acc, fn) => fn(acc), arg)
 
 export const pipe = <T>(arg: T, ...fns: Chain<T>) =>
   fns.reduce((acc, fn) => fn(acc), arg)
-// export const or =
-//   <L, R>(left: Fn<L>, right: Fn<R>) =>
-//   (arg: L | R): L | R => {
-//     const errors = []
-//   }
+
 interface WithLength {
   length: number
 }
-export const min_length = (len: number) => {
+export const min_length = (len: number, errorMessage?: string) => {
   function check_length<T extends WithLength>(arg: T): T
   function check_length<T>(arg: T extends string ? T : T[]) {
-    if (arg.length > len) throw new Error('ssss')
+    if (arg.length > len) throw new ValidationError(errorMessage || 'Invalid length')
     return arg
   }
   return check_length
-}
-
-export const concat_arr = <T>(val: T) => {
-  return (arg: T[]) => {
-    //   const arr = arg as T extends T[] ? T[] : never
-    //   arg.push(val)
-    return [...arg, val]
-  }
 }
