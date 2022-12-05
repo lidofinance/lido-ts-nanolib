c9f6c7043dd260204539ad412bf46c5592af1b58
Eddort <webisforce@gmail.com>
2022-12-05 22:09:54 +0400

    fix: object error handling

--- src/validator/makers.ts
+++ src/validator/makers.ts
@@ -1,21 +1,25 @@
-import { makeError, ValidationError } from './errors.js'
+import { ValidationError } from './errors.js'
 
 export const make = <T>(
   parseFn: (input: unknown, errorMessage?: string | number) => T
 ) => {
-  return (str?: unknown, errorMessage?: string | number) => {
-    if (typeof str === 'undefined') makeError(errorMessage, 'Empty value')
-    return parseFn(str, errorMessage)
+  return (input?: unknown, errorMessage?: string | number) => {
+    if (typeof input === 'undefined')
+      throw new ValidationError(errorMessage, 'Empty value')
+    return parseFn(input, errorMessage)
   }
 }
 
 export const makeDeep = <T>(
   parseFn: (input: unknown, errorMessage?: string) => T
 ) => {
-  return <B>(str: unknown, cb: (arg: T) => B, errorMessage?: string) => {
-    if (typeof str === 'undefined')
-      throw new ValidationError(errorMessage || 'Empty value')
-
-    return cb(parseFn(str, errorMessage))
+  return <B>(input: unknown, cb: (arg: T) => B, errorMessage?: string) => {
+    if (typeof input === 'undefined')
+      throw new ValidationError(errorMessage || 'Empty value', input)
+    try {
+      return cb(parseFn(input, errorMessage))
+    } catch (error) {
+      throw new ValidationError(error.message, input)
+    }
   }
 }
